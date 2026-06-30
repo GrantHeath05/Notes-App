5d8ee1e824a9732caf324aa38e8458fa2aab6ce7
Douglas Grant Heath <156680651+GrantHeath05@users.noreply.github.com>
2026-06-30 17:43:47 -0400

    Maven Setup Start Point
    Empty Maven Project

---
+++ src/main/java/com/grant/notesapp/NotesApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grant.notesapp;
+
+/**
+ *
+ * @author grant
+ */
+public class NotesApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
